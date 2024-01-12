a8229438b520b9a4bfa7e2594d174a50cfb1f6b4
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-12 11:12:22 -0500

    add more functionality

--- client/cmd/auth.go
+++ client/cmd/auth.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/user"
 
 	authz "github.com/CHESSComputing/golib/authz"
 	services "github.com/CHESSComputing/golib/services"
@@ -18,13 +19,51 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var envTokens []string = []string{"CHESS_TOKEN", "CHESS_WRITE_TOKEN", "CHESS_DELETE_TOKEN"}
+
 // helper function to provide usage of meta option
-func authUsage() {
-	fmt.Println("client auth kerberos")
-	fmt.Println("client auth token")
+func authUsage() string {
+	var out string
+	out += fmt.Sprintf("client auth token <scope: read|write|delete> <--kfile=keytab>\n")
+	out += fmt.Sprintf("client auth view <--token=token or file>\n\n")
+	out += fmt.Sprintf("defaults: token generated with read scope\n")
+	out += fmt.Sprintf("          kfile is /tmp/krb5cc_<UID>\n")
+	out += fmt.Sprintf("\n")
+	out += fmt.Sprintf("Examples: \n")
+	out += fmt.Sprintf("# generate read token\n")
+	out += fmt.Sprintf("client auth token read\n")
+	out += fmt.Sprintf("\n")
+	out += fmt.Sprintf("# generate read token from specific /path/keytab file\n")
+	out += fmt.Sprintf("client auth token read -kfile=/path/keytab\n")
+	out += fmt.Sprintf("\n")
+	out += fmt.Sprintf("# generate write token\n")
+	out += fmt.Sprintf("client auth token write\n")
+	out += fmt.Sprintf("\n")
+	out += fmt.Sprintf("# view provided token=abc...xyz\n")
+	out += fmt.Sprintf("client auth token view --token=abc...xyz\n")
+	out += fmt.Sprintf("\n")
+	out += fmt.Sprintf("# view existing token stored in /tmp/token file\n")
+	out += fmt.Sprintf("client auth token view --token=/tmp/token\n")
+	out += fmt.Sprintf("\n")
+	out += fmt.Sprintf("# view existing token stored in %s\n", envTokens)
+	out += fmt.Sprintf("client auth token view\n")
+	return out
+}
+
+// helper function to return user's key file name
+func keyFile() string {
+	u, err := user.Current()
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		os.Exit(1)
+	}
+	return fmt.Sprintf("/tmp/krb5cc_%s", u.Uid)
 }
 
 func requestToken(scope, fname string) (string, error) {
+	if fname == "" {
+		fname = keyFile()
+	}
 	var token string
 	user, ticket := getKerberosTicket(fname)
 	rec := authz.Kerberos{
@@ -67,17 +106,32 @@ func requestToken(scope, fname string) (string, error) {
 	return token, errors.New(msg)
 }
 
-func inspectAllTokens() {
-	inspectToken("CHESS_TOKEN")
-	inspectToken("CHESS_WRITE_TOKEN")
+func inspectAllTokens(tkn string) {
+	var token string
+	if tkn != "" {
+		token = utils.ReadToken(tkn)
+		inspectToken(token)
+		return
+	}
+	fmt.Println("No input token is provided, will lookup them from env...")
+	for _, env := range envTokens {
+		token = utils.ReadToken(os.Getenv(env))
+		if token != "" {
+			s := fmt.Sprintf("%s: %s", env, token)
+			fmt.Println("")
+			fmt.Println(s)
+			inspectToken(token)
+		}
+	}
 }
 
-func inspectToken(tkn string) {
-	token := utils.ReadToken(os.Getenv(tkn))
-	claims := authz.TokenClaims(token, _srvConfig.Authz.ClientID)
+func inspectToken(token string) {
+	claims, err := authz.TokenClaims(token, _srvConfig.Authz.ClientID)
 	rclaims := claims.RegisteredClaims
 	fmt.Println()
-	fmt.Println("Read token   : ", tkn)
+	if err != nil {
+		fmt.Println("ERROR        : ", err)
+	}
 	fmt.Println("AccessToken  : ", token)
 	fmt.Println("Issuer       : ", rclaims.Issuer)
 	fmt.Println("Subject      : ", rclaims.Subject)
@@ -90,26 +144,26 @@ func authCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "auth",
 		Short: "client authentication/authorization commands",
-		Long:  "client authentication/authorization commands\n" + doc,
+		Long:  "client authentication/authorization commands\n" + doc + "\n" + authUsage(),
 		Run: func(cmd *cobra.Command, args []string) {
+			tkn, _ := cmd.Flags().GetString("token")
+			fname, _ := cmd.Flags().GetString("kfile")
 			if len(args) == 0 {
-				authUsage()
+				fmt.Print(authUsage())
 			} else if args[0] == "token" {
 				attr := args[1]
 				var token, tokenKind string
 				var err error
-				if attr == "view" || attr == "inspect" {
-					inspectAllTokens()
+				if attr == "view" {
+					inspectAllTokens(tkn)
 					return
 				}
-				//                 log.Println("#### args", args, attr, args[len(args)-1])
 				if attr == "write" {
-					token, err = requestToken("write", args[len(args)-1])
 					tokenKind = "write"
 				} else {
-					token, err = requestToken("read", args[len(args)-1])
 					tokenKind = "read"
 				}
+				token, err = requestToken(attr, fname)
 				if err != nil {
 					exit("unable to get valid token", err)
 				}
@@ -124,5 +178,11 @@ func authCommand() *cobra.Command {
 			}
 		},
 	}
+	cmd.PersistentFlags().String("kfile", "", "Kerberos file to use")
+	cmd.PersistentFlags().String("token", "", "token file or token string")
+	cmd.SetUsageFunc(func(*cobra.Command) error {
+		fmt.Println(authUsage())
+		return nil
+	})
 	return cmd
 }
