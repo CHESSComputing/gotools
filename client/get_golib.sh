33bce3032dabe0339740a7a3ffc1c048531fd650
Valentin Kuznetsov <vkuznet@gmail.com>
2024-02-02 09:04:51 -0500

    rename client to foxden

--- client/get_golib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ ! -d ../../golib ]; then
-    cd ../../
-    echo "clone https://github.com/CHESSComputing/golib.git"
-    git clone https://github.com/CHESSComputing/golib.git
-    cd -
-fi
