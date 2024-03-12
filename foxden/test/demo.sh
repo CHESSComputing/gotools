390af3c62304208ffadf89ae92498257b26e02fc
Valentin Kuznetsov <vkuznet@gmail.com>
2024-03-12 13:06:42 -0400

    Improve demo script

--- foxden/test/demo.sh
+++ foxden/test/demo.sh
@@ -6,17 +6,19 @@ schema=ID3A
 
 echo
 echo "get write token"
-token=`./foxden auth token write /tmp/krb5cc_502 | head -1`
+token=`./foxden auth token write | head -1`
 export CHESS_WRITE_TOKEN=$token
 # echo "CHESS_WRITE_TOKEN=$CHESS_WRITE_TOKEN"
 echo
 echo "get read token"
-token=`./foxden auth token /tmp/krb5cc_502 | head -1`
+token=`./foxden auth token | head -1`
 export CHESS_TOKEN=$token
 # echo "CHESS_TOKEN=$CHESS_TOKEN"
 echo
 echo "view issued tokens"
 ./foxden auth token view
+
+sleep 5
 
 echo
 echo "remove dbs.db"
@@ -29,20 +31,30 @@ cat > /tmp/cleanup.js << EOF
 use chess;
 db.meta.remove({});
 EOF
-mongo --port 8230 < /tmp/cleanup.js
+mongo --port 8230 < /tmp/cleanup.js 2>&1 1>& /dev/null
 
 echo
 echo "+++ Add new meta-data record $idir/ID3A-meta1.json"
+echo "./foxden meta add $schema $idir/ID3A-meta1.json"
 ./foxden meta add $schema $idir/ID3A-meta1.json
+
+sleep 2
 
 echo
 echo "+++ Add new dbs-data record $idir/ID3A-dbs1.json"
+echo "./foxden prov add $idir/ID3A-dbs1.json"
 ./foxden prov add $idir/ID3A-dbs1.json
+
+sleep 2
 
 echo
 echo "+++ search for all records"
+echo "./foxden search {}"
 ./foxden search {}
+
+sleep 2
 
 echo
 echo "+++ view record /beamline=3a/btr=3731-b/cycle=2023-3"
+echo "./foxden view /beamline=3a/btr=3731-b/cycle=2023-3"
 ./foxden view /beamline=3a/btr=3731-b/cycle=2023-3
