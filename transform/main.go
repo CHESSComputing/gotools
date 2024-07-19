1aee7128998532cf2844c01a4ff18a48263ee406
Valentin Kuznetsov <vkuznet@gmail.com>
2024-07-19 09:50:01 -0400

    Add transformRecord function

--- transform/main.go
+++ transform/main.go
@@ -15,9 +15,15 @@ func main() {
 	flag.StringVar(&fileIn, "fileIn", "", "input file name")
 	var fileOut string
 	flag.StringVar(&fileOut, "fileOut", "", "input file name")
+	var schema bool
+	flag.BoolVar(&schema, "schema", false, "transform schema meta-data record")
 	flag.Parse()
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	migrate(fileIn, fileOut)
+	if schema {
+		migrate(fileIn, fileOut)
+	} else {
+		migrateRecord(fileIn, fileOut)
+	}
 }
 
 // MetaSchema describes FOXDEN meta-data schema record
@@ -66,6 +72,46 @@ func migrate(fileIn, fileOut string) {
 	}
 	defer fout.Close()
 	data, err = json.MarshalIndent(out, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fout.Write(data)
+}
+
+func migrateRecord(fileIn, fileOut string) {
+	if fileIn == "" {
+		log.Fatal("empty input file name")
+	}
+	if fileOut == "" {
+		log.Fatal("empty output file name")
+	}
+
+	file, err := os.Open(fileIn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var record map[string]any
+	err = json.Unmarshal(data, &record)
+	if err != nil {
+		log.Println("record", string(data))
+		log.Fatal(err)
+	}
+	nrec := make(map[string]any)
+	for key, val := range record {
+		nkey := utils.CamelCaseToSnakeCase(key)
+		nrec[nkey] = val
+	}
+	fout, err := os.Create(fileOut)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fout.Close()
+	data, err = json.MarshalIndent(nrec, "", "  ")
 	if err != nil {
 		log.Fatal(err)
 	}
