c668db47c71e7ef0da3ba0dec5c3905ea05c3090
Valentin Kuznetsov <vkuznet@gmail.com>
2024-10-24 07:55:01 -0400

    Simplify printConfig to use _srvConfig

--- foxden/cmd/config.go
+++ foxden/cmd/config.go
@@ -6,11 +6,7 @@ package cmd
 //
 import (
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 
-	srvConfig "github.com/CHESSComputing/golib/config"
 	"github.com/spf13/cobra"
 )
 
@@ -20,38 +16,8 @@ func configUsage() {
 }
 
 func printConfig(args []string) {
-	var fname, cfile string
-	home, err := os.UserHomeDir()
-	if err == nil {
-		fname = filepath.Join(home, ".foxden.yaml")
-	}
-	// determine which configuration file we use
-	if _, err := os.Stat(fname); err == nil {
-		cfile = fname
-	} else {
-		if _, err := os.Stat(os.Getenv("FOXDEN_CONFIG")); err == nil {
-			cfile = os.Getenv("FOXDEN_CONFIG")
-		} else {
-			fname = "/nfs/chess/user/chess_chapaas/.foxden.yaml"
-			if _, err := os.Stat(fname); err == nil {
-				cfile = fname
-			} else {
-				fname = `\\chesssamba.classe.cornell.edu\user\chess_chapaas\.foxden.yaml`
-				if _, err := os.Stat(fname); err == nil {
-					cfile = fname
-				} else {
-					msg := "FOXDEN configuration file is not found"
-					log.Fatal(msg)
-				}
-			}
-		}
-	}
-	config, err := srvConfig.ParseConfig(cfile)
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
-	fmt.Printf("Configuration file: %s\n", cfile)
-	fmt.Println(config.String())
+	fmt.Printf("Configuration file: %s\n", cfgFile)
+	fmt.Println(_srvConfig.String())
 }
 
 func configCommand() *cobra.Command {
