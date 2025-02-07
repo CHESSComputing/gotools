bad05a5a2639c51328ea33cc9970df34fa4f9eba
Valentin Kuznetsov <vkuznet@gmail.com>
2025-02-07 08:04:15 -0500

    add options -updateGlobus,updateDID,updateBTR,updateSID

--- metaupdate/main.go
+++ metaupdate/main.go
@@ -14,12 +14,27 @@ func main() {
 	flag.StringVar(&dbCol, "dbCol", "", "mongodb collection")
 	var execute bool
 	flag.BoolVar(&execute, "execute", false, "execute actions on MongoDB records")
+	var updateGlobus bool
+	flag.BoolVar(&updateGlobus, "updateGlobus", false, "update Globus links on MongoDB records")
+	var updateDID bool
+	flag.BoolVar(&updateDID, "updateDID", false, "update DIDs on MongoDB records")
+	var updateBTR bool
+	flag.BoolVar(&updateBTR, "updateBTR", false, "updateBTRs on MongoDB records")
+	var updateSID bool
+	flag.BoolVar(&updateSID, "updateSID", false, "updateSIDs on MongoDB records")
 	flag.Parse()
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	log.Println("### update globus links")
-	updateGlobusLinks(uri, dbName, dbCol, execute)
-	log.Println("### update DIDs")
-	updateDIDs(uri, dbName, dbCol, execute)
-	log.Println("### update BTRs")
-	updateBTRs(uri, dbName, dbCol, execute)
+	if updateGlobus {
+		log.Println("### update globus links")
+		updateGlobusLinks(uri, dbName, dbCol, execute)
+	} else if updateDID {
+		log.Println("### update DIDs")
+		updateDIDs(uri, dbName, dbCol, execute)
+	} else if updateBTR {
+		log.Println("### update BTRs")
+		updateBTRs(uri, dbName, dbCol, execute)
+	} else if updateSID {
+		log.Println("### update SIDs")
+		updateSIDs(uri, dbName, dbCol, execute)
+	}
 }
