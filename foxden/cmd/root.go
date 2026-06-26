626ce23e81a1c2f81b0c02dca2b7725f0aeee878
Valentin Kuznetsov <vkuznet@gmail.com>
2026-06-26 14:08:16 -0400

    add new tmpl command

--- foxden/cmd/root.go
+++ foxden/cmd/root.go
@@ -74,6 +74,7 @@ func init() {
 	rootCmd.AddCommand(userMetaCommand())
 	rootCmd.AddCommand(fabricCommand())
 	rootCmd.AddCommand(validateCommand())
+	rootCmd.AddCommand(tmplCommand())
 }
 
 func initConfig() {
